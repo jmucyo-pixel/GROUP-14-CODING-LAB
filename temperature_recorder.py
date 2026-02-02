fc38f9fd249453775690dc31a48d4719b731a7a1
Elvis Manzi <d.manzimul@alustudent.com>
2026-02-02 13:30:43 +0200

    Set up active_logs directory and temperature log file

--- temperature_recorder.py
+++ temperature_recorder.py
@@ -2,3 +2,15 @@ import time
 import random
 from datetime import datetime
 import os
+
+# -----------------------------
+
+# COMMENT 1: Ensure the active_logs directory exists
+
+# This is where all temperature log files will be stored
+
+os.makedirs("active_logs", exist_ok=True)
+
+# Path to the log file
+
+log_file = "active_logs/temperature.log"
